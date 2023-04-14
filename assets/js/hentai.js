cbe0035a4ca95e67e1d842e5bd9b46f8ad7f4639
0yen4snafu <0yen4snafu@users.noreply.github.com>
2023-04-14 22:27:43 +0200

    hm

---
+++ assets/js/hentai.js
@@ -0,0 +1,7 @@
+var h = Math.floor(Math.random() * 1001);
+if (h >= 999) {
+    document.write('<object title="site views!!" data="https://count.getloli.com/get/@0yen4snafu.github.io?theme=moebooru-h"></object>');
+}
+else {
+    document.write('<object title="site views!!" data="https://count.getloli.com/get/@0yen4snafu.github.io?theme=rule34"></object>');
+}
