710dabc1410c647c97b4354c403848177b6da16a
0yen4snafu <0yen4snafu@users.noreply.github.com>
2023-05-02 21:52:57 +0200

    s

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,5 +1,11 @@
-var fuck = ["stupid fuck", "give it up", "bitch"];
+var fuck = [
+    "Enough motivation for nothing.", 
+    "Have you noticed me?", 
+    "Pretend like you never saw me.",
+    "I want to go home.",
+    "Don't bother me."
+];
 
 var me = Math.floor(fuck.length * Math.random()); 
 
-document.write("<p role='quote'><q>" + fuck[me]  + "</q></p>");
+document.write("<p role='quote'>— <q>" + fuck[me]  + "</q></p>");
