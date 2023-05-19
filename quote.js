e8f2585c1a0d04de6afaa53962b19787e03647bf
0yen4snafu <0yen4snafu@users.noreply.github.com>
2023-05-20 01:09:44 +0200

    adsasdasdasdasd

--- quote.js
+++ quote.js
@@ -4,7 +4,9 @@ var fuck = [
     "Pretend like you never saw me.",
     "I want to go home.",
     "Don't bother me.",
-    "You shouldn't have expected anything from me."
+    "You shouldn't have expected anything from me.",
+    "I'm so glad we live only once, I can't do this again",
+    "I really don't care"
 ];
 
 var me = Math.floor(fuck.length * Math.random()); 
